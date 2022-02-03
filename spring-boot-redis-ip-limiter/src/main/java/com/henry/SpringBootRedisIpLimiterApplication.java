46f03986ee1f8c00f494492f0cc310c1fa71d257
huangjiawen <123456zaq@>
2022-02-04 00:15:37 +0800

    SpringBoot整合redis，利用aop+自定义注解+redis lua脚本，实现ip的限流

---
+++ spring-boot-redis-ip-limiter/src/main/java/com/henry/SpringBootRedisIpLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.henry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisIpLimiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisIpLimiterApplication.class, args);
+    }
+
+}
